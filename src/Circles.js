342768c149061df2658edc06f7a4b3831570159a
yonatanmk <yonatanmk@gmail.com>
2023-02-19 15:03:14 -0800

    refactor

---
+++ src/Circles.js
@@ -0,0 +1,39 @@
+import { useRef, useEffect, useState } from "react";
+import { select } from "d3";
+
+const defaultData = [25, 30, 45, 60, 20];
+
+function Circles() {
+  const [data, setData] = useState(defaultData);
+  const svgRef = useRef();
+  useEffect(() => {
+    const svg = select(svgRef.current);
+    svg
+      .selectAll("circle")
+      .data(data)
+      .join("circle")
+      // .join(
+      //   (enter) => enter.append("circle").attr("class", "new")
+      //   // (update) => update.attr("class", "updated")
+      //   // (exit) => exit.remove()
+      // )
+      .attr("r", (value) => value)
+      .attr("cx", (value) => value * 2)
+      .attr("cy", (value) => value * 2)
+      .attr("stroke", "red");
+  }, [data]);
+  return (
+    <>
+      <svg ref={svgRef}></svg>
+      <br />
+      <button onClick={() => setData((prev) => prev.map((x) => x + 5))}>
+        Update Data
+      </button>
+      <button onClick={() => setData((prev) => prev.filter((x) => x < 35))}>
+        Filter Data
+      </button>
+    </>
+  );
+}
+
+export default Circles;
